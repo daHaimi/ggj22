97873c24ae26a8ced38e1dedd093747bdb01100d
rellig <rellig@minad.de>
2022-01-30 02:38:03 +0100

    use hole marker

--- Assets/ItemsMisc/raw/Pfeil.cs
+++ Assets/ItemsMisc/raw/Pfeil.cs
@@ -1,17 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using SuperTiled2Unity;
 using UnityEngine;
 
 public class Pfeil : MonoBehaviour
 {
     public float damage;
     public float ttl;
+
+    private Collider2D _collider2D;
     
     // Start is called before the first frame update
     void Start()
     {
         gameObject.tag = "Bullet";
+        _collider2D = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -19,5 +23,14 @@ public class Pfeil : MonoBehaviour
     {
         ttl -= Time.deltaTime;
         if (ttl <= 0) Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SuperObject sObject = collision.gameObject.GetComponent<SuperObject>();
+        if (sObject && sObject.m_Type == "hole")
+        {
+            Physics2D.IgnoreCollision(collision.collider, collision.otherCollider);
+        }
     }
 }
